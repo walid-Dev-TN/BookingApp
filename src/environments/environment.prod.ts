d9501b22740bb79aea69210dbdcf873df68eb011
Walid-Dev-TUN <wal.said2017@gmail.com>
2019-11-07 16:37:29 +0100

    PushNotificationsRevision-environnement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,6 @@ export const environment = {
     projectId: "blassa-df986",
     storageBucket: "blassa-df986.appspot.com",
     messagingSenderId: "400236234696",
-    appId: "1:400236234696:web:2bd8c5fcac28140e2e62fd",
     vapidKey: 'BKCkiGAGSvEL4M9MiT3PwOea3wed60EFMDa18P9RpY0HTt-pg_B5UpZIzld0h85iTAarhFY8TXqHes09lnM6Fns'
   }
 };
