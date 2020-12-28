aad542e8fabe5e116baa2e2d02a7c4e538549700
walid-Dev-TN <57041362+walid-Dev-TN@users.noreply.github.com>
2020-12-28 11:13:41 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,7 @@
 
 export const environment = {
   production: false,
-  googleMapsAPIKey: "AIzaSyDDNQ5XcndVgzuKEGT7b5WXFAF5amAIF8A",
-  firebase: {
-    apiKey: "AIzaSyB0bupmph7F3yazBwjcXj1GrfU3n6l-iKo",
-    authDomain: "blassa-df986.firebaseapp.com",
-    databaseURL: "https://blassa-df986.firebaseio.com",
-    projectId: "blassa-df986",
-    storageBucket: "blassa-df986.appspot.com",
-    messagingSenderId: "400236234696",
-    appId: "1:400236234696:web:2bd8c5fcac28140e2e62fd",
-    vapidKey: 'BKCkiGAGSvEL4M9MiT3PwOea3wed60EFMDa18P9RpY0HTt-pg_B5UpZIzld0h85iTAarhFY8TXqHes09lnM6Fns'
-  }
+    }
 };
 
 /*
